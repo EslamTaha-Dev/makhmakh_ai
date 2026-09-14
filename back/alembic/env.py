4541369b7cb7233543a346fb221292366b16b823
Abdo Emad <studentabdo83@gmail.com>
2026-09-14 18:39:42 +0300

    fix: change database_url to DATABASE_URL in alembic env

--- back/alembic/env.py
+++ back/alembic/env.py
@@ -26,7 +26,7 @@ target_metadata = Base.metadata
 def run_migrations_offline() -> None:
     """Run migrations in offline mode."""
 
-    url = settings.database_url
+    url = settings.DATABASE_URL
 
     context.configure(
         url=url,
@@ -48,7 +48,7 @@ def run_migrations_online() -> None:
     if configuration is None:
         configuration = {}
 
-    configuration["sqlalchemy.url"] = settings.database_url
+    configuration["sqlalchemy.url"] = settings.DATABASE_URL
     connectable = engine_from_config(
         configuration,
         prefix="sqlalchemy.",
